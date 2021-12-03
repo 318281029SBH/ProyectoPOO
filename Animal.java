f00db26a7414f49b0ad459d5fe78bfa9c7f9c5d4
318281029SBH <72660236+318281029SBH@users.noreply.github.com>
2021-12-02 19:22:16 -0600

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,11 @@
+package frida;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Animal {
+	public boolean estadoDeAdopcion;
+	public boolean constanciaSalud;
+	public HistoriaCLinica certificado_salud;
+	public Procedencia procedencia;
+}
